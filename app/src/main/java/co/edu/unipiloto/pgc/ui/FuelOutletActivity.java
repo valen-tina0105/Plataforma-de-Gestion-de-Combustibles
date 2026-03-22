6929c7158b649007e60a300ac46328e5392d9ae8
Cristian Cortés <bejaranno05cortes@gmail.com>
2026-03-22 16:35:12 -0500

    mejora interfaz FuelOutletActivity

--- app/src/main/java/co/edu/unipiloto/pgc/ui/FuelOutletActivity.java
+++ app/src/main/java/co/edu/unipiloto/pgc/ui/FuelOutletActivity.java
@@ -3,12 +3,17 @@ package co.edu.unipiloto.pgc.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -19,12 +24,14 @@ import co.edu.unipiloto.pgc.model.Register;
 import co.edu.unipiloto.pgc.model.Rule;
 import co.edu.unipiloto.pgc.model.Transaction;
 import co.edu.unipiloto.pgc.model.User;
+import co.edu.unipiloto.pgc.ui.adapters.RegisterAdapter;
 
 public class FuelOutletActivity extends BaseActivity {
 
     private ArrayList<Register> registers;
     private RegisterDAO registerDAO;
     private User user;
+    private RegisterAdapter adapterRegistros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +41,35 @@ public class FuelOutletActivity extends BaseActivity {
         user = (User) intent.getSerializableExtra("user");
         registerDAO = new RegisterDAO(this);
         registers = registerDAO.getAllRegisters(user);
-        TextView textoRegistros = findViewById(R.id.textoRegistros);
-        String textoCompleto="";
-        for(int i=0; i<registers.size(); i++) {
-            textoCompleto += "Entrada " + registers.get(i).getId() + ": Fecha: " + registers.get(i).getFechaFormateada()
-                    + " Tipo Combustible: " + registers.get(i).getTipoCombustible()
-                    + " Cantidad: " +registers.get(i).getCantidad()
-                    + " Registrado por: " + registers.get(i).getEstacion().getUsername()
-                + "\n";
-        }
-        textoRegistros.setText(textoCompleto);
+
+        RecyclerView listaRegistros = findViewById(R.id.listaRegistros);
+        listaRegistros.setLayoutManager(new LinearLayoutManager(this));
+
+        adapterRegistros = new RegisterAdapter(registers);
+        listaRegistros.setAdapter(adapterRegistros);
+
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(this::onEntryRegister);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            Intent sendIntent;
+            if (id == R.id.nav_calcular) {
+                sendIntent = new Intent(this, PriceCalculatorActivity.class);
+                sendIntent.putExtra("user", user);
+                startActivity(sendIntent);
+                finish();
+                return true;
+            } else if (id == R.id.nav_historial) {
+                sendIntent = new Intent(this, FuelHistoryActivity.class);
+                sendIntent.putExtra("user", user);
+                startActivity(sendIntent);
+                finish();
+                return true;
+            } else return id == R.id.nav_registrar;
+        });
+
         ImageButton btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
         btnCerrarSesion.setOnClickListener(this::onLogOut);
     }
@@ -55,41 +81,13 @@ public class FuelOutletActivity extends BaseActivity {
             return;
         }
 
-        TextView textoRegistros = findViewById(R.id.textoRegistros);
         Register register = new Register();
         register.setTipoCombustible(tipoCombustible.getSelectedItem().toString());
         register.setCantidad(Integer.parseInt(textoCantidad.getText().toString()));
         register.setEstacion(user);
         registers.add(register);
         registerDAO.insertarRegistro(register);
-        String textoCompleto="";
-        for(int i=0; i < registers.size(); i++) {
-            textoCompleto += "Entrada " + (i + 1) + ": Fecha: " + registers.get(i).getFechaFormateada()
-                    + " Tipo Combustible: " + registers.get(i).getTipoCombustible()
-                    + " Cantidad: " +registers.get(i).getCantidad()
-                    + " Registrado por: " + registers.get(i).getEstacion().getUsername()
-                    + "\n";
-        }
-        textoRegistros.setText(textoCompleto);
-        textoCantidad.setText("");
+
     }
 
-    public void onChangeActivity(View view){
-        Spinner actividades = findViewById(R.id.actividades);
-        Intent intent;
-        switch (actividades.getSelectedItem().toString()){
-            case "Calcular Precio":
-                intent = new Intent(this, PriceCalculatorActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
-            case "Registar Entrada":
-                break;
-            case "Consultar Historial":
-                intent = new Intent(this, FuelHistoryActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
-        }
-    }
 }
---
+++ app/src/main/java/co/edu/unipiloto/pgc/ui/adapters/RegisterAdapter.java
@@ -0,0 +1,69 @@
+package co.edu.unipiloto.pgc.ui.adapters;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import co.edu.unipiloto.pgc.R;
+import co.edu.unipiloto.pgc.model.Movement;
+import co.edu.unipiloto.pgc.model.Register;
+
+public class RegisterAdapter extends RecyclerView.Adapter<RegisterAdapter.RegistersViewHolder> {
+
+    private ArrayList<Register> registers;
+
+    public RegisterAdapter(ArrayList<Register> registers) {
+        this.registers = registers;
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void updateList(ArrayList<Register> newList) {
+        this.registers = newList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public RegistersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.register_item, parent, false);
+        return new RegistersViewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull RegistersViewHolder holder, int position) {
+        holder.viewRegistro.setText("Registro " + registers.get(position).getId());
+        holder.viewEstacion.setText("Estacion: " + registers.get(position).getEstacion().getUsername());
+        holder.viewTipoCombustible.setText("Tipo de combustible: " + registers.get(position).getTipoCombustible());
+        holder.viewCantidad.setText("Cantidad: " + registers.get(position).getCantidad());
+        holder.viewFecha.setText("Fecha: " + registers.get(position).getFechaFormateada());
+    }
+
+    @Override
+    public int getItemCount() {
+        return registers.size();
+    }
+
+    public class RegistersViewHolder extends RecyclerView.ViewHolder {
+
+        TextView viewRegistro, viewEstacion, viewTipoCombustible, viewCantidad, viewFecha;
+
+        public RegistersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            viewRegistro = itemView.findViewById(R.id.viewRegistro);
+            viewEstacion = itemView.findViewById(R.id.viewEstacion);
+            viewTipoCombustible = itemView.findViewById(R.id.viewTipoCombustible);
+            viewCantidad = itemView.findViewById(R.id.viewCantidad);
+            viewFecha = itemView.findViewById(R.id.viewFecha);
+        }
+    }
+}
+
+
