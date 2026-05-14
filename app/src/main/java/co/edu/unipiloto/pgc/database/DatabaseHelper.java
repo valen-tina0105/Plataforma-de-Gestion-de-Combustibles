d77cdbb20c839e5933b757b2a961c4a4663a4145
Cristian Cortés <bejaranno05cortes@gmail.com>
2026-05-14 10:40:23 -0500

    refactor inserts of stations

--- app/src/main/java/co/edu/unipiloto/pgc/database/DatabaseHelper.java
+++ app/src/main/java/co/edu/unipiloto/pgc/database/DatabaseHelper.java
@@ -11,7 +11,7 @@ import java.io.InputStreamReader;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "PGC_v22";
+    private static final String DB_NAME = "PGC_v28";
     private Context context;
 
     public DatabaseHelper(Context context) {
